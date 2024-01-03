dd274e581682c202dc5e5eaa5cc372b6983e5a55
minhnhut1712 <nminhnhut0112@gmail.com>
2024-01-03 22:02:44 +0700

    api card

--- src/apis/cards.api.js
+++ src/apis/cards.api.js
@@ -9,3 +9,27 @@ export const updateCardAPI = async (id, data) => {
   const res = await http.put(`v1/cards/${id}`, data)
   return res.data
 }
+
+export const changeCoverAPI = async (id, data) => {
+  const res = await http.post(`v1/cards/${id}`, data, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+  return res.data
+}
+
+export const removeCoverAPI = async (id) => {
+  const res = await http.delete(`v1/cards/removeItem/${id}`)
+  return res.data
+}
+
+export const cardUploadFileAPI = async (id, data) => {
+  const res = await http.post(`v1/cards/${id}/upload`, data, {
+    headers: { 'Content-Type': 'multipart/form-data' }
+  })
+  return res.data
+}
+
+export const deleteCardAPI = async (id) => {
+  const res = await http.delete(`v1/cards/${id}`)
+  return res.data
+}
